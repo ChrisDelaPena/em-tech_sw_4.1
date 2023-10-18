4d0e3d723b6c99cee7082b35a55595795579edb5
allenbayoneta <ajpbayoneta@gmail.com>
2023-10-18 23:29:24 +0800

    adding init messaging codes

---
+++ messaging/App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Status from './componets/Status';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Status />
+      <Text>HOA 4.1</Text>
+      <Text>Allen Bayoneta</Text>
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: 'rgba(0,0,0,0.04)',
+    backgroundColor: '#fff',
+  },
+});
